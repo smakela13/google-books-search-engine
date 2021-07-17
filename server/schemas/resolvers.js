f2e6791b3b488f50372f3b42e9456fad949dc1c0
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-16 22:45:46 -0400

    Updated the resolvers - Added node_modules to gitignore

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,30 +4,63 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
 	Query: {
-		user: async (parent, { username }) => {
-			return User.findOne({ username });
+		user: async (parent, { args }) => {
+			const foundUser = await User.findOne({args}).populate('savedBooks')
+			return foundUser;
+		},
+		user: async () => {
+			return User.find({});
 		},
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({_id: context.user._id}).populate('savedBooks')
+        return User.findOne({_id: context.user._id})
       }
       throw new AuthenticationError('You need to be logged in!');
     }
-  },
-	Mutation: {
-		createMatchup: async (parent, args) => {
-			const matchup = await Matchup.create(args);
-			return matchup;
-		},
-		createVote: async (parent, { _id, techNum }) => {
-			const vote = await Matchup.findOneAndUpdate(
-				{ _id },
-				{ $inc: { [`tech${techNum}_votes`]: 1 } },
-				{ new: true }
-			);
-			return vote;
-		},
 	},
-};
+	
+	Mutation: {
+		login: async (parent, {password, email}) => {
+			const user = await User.findOne({email});
+
+			if (!user) {
+				throw new AuthenticationError('Please try again with valid login')
+			}
+			const correctPw = await user.isCorrectPassword(password);
+			
+			if (!correctPw) {
+			throw new AuthenticationError('Please try again with valid login');
+			}
+			
+			const token = signToken(user);
+			return {token, user};
+		},
+		addUser: async (parent, {username, email, password}) => {
+			const user = User.create({username, email, password});
+			const token = signToken(user);
+			return {token, user};
+		},
+		saveBook: async (parent, args, context) => {
+			if (context.user) {
+				const updatedUser = await User.findByIdAndUpdate({_id: context.user._id},
+					{$addToSet: {savedBooks: args.input}},
+					{new: true, runValidators: true}
+			);
+			return updatedUser;
+			}
+			throw new AuthenticationError('You need to be logged in!');
+		},
+		removeBook: async (parent, {bookId}, context) => {
+			if (context.user) {
+				const updatedUser = await User.findByOneAndUpdate({_id: context.user._id},
+					{$pull: {savedBooks: {bookId}}},
+					{new: true}
+				);
+				return updatedUser;
+			}
+			throw new AuthenticationError('You need to be logged in!');
+		}
+	}
+}
 
 module.exports = resolvers;
